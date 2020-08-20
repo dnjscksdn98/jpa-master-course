c9362bdbc28c942676fb554d8156264021579132
Alex Won <dnjscksdn98@gmail.com>
2020-08-20 17:44:12 +0900

    feat: create queries with entity relationships using jpql

--- src/main/java/com/udemy/jpa/JpaApplication.java
+++ src/main/java/com/udemy/jpa/JpaApplication.java
@@ -1,7 +1,5 @@
 package com.udemy.jpa;
 
-import com.udemy.jpa.models.FullTimeEmployee;
-import com.udemy.jpa.models.PartTimeEmployee;
 import com.udemy.jpa.repository.EmployeeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
 
 @SpringBootApplication
 public class JpaApplication implements CommandLineRunner {
@@ -26,21 +23,6 @@ public class JpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		employeeRepository.insert(
-				FullTimeEmployee
-						.builder()
-						.name("Jack")
-						.salary(new BigDecimal("10000"))
-						.build());
 
-		employeeRepository.insert(
-				PartTimeEmployee
-						.builder()
-						.name("Jill")
-						.hourlyWage(new BigDecimal("50"))
-						.build());
-
-		logger.info(String.format("Full Time Employees -> {%s}", employeeRepository.retrieveAllFullTimeEmployees()));
-		logger.info(String.format("Part Time Employees -> {%s}", employeeRepository.retrieveAllPartTimeEmployees()));
 	}
 }
--- src/test/java/com/udemy/jpa/repository/JPQLTests.java
+++ src/test/java/com/udemy/jpa/repository/JPQLTests.java
@@ -2,6 +2,7 @@ package com.udemy.jpa.repository;
 
 import com.udemy.jpa.JpaApplication;
 import com.udemy.jpa.models.Course;
+import com.udemy.jpa.models.Student;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
@@ -50,5 +51,80 @@ public class JPQLTests {
         TypedQuery<Course> query = entityManager.createNamedQuery("query_get_all_courses", Course.class);
         List<Course> courses = query.getResultList();
         logger.info(String.format("SELECT C FROM Course C -> %s", courses));
+    }
+
+    /**
+     * Queries with entity relationships using JPQL
+     */
+
+    @Test
+    public void findCoursesWithoutStudents() {
+        TypedQuery<Course> query = entityManager.createQuery("SELECT C FROM Course C WHERE C.students is empty", Course.class);
+        List<Course> courses = query.getResultList();
+        logger.info(String.format("Courses without students -> {%s}", courses));
+    }
+
+    @Test
+    public void findCoursesAtLeastTwoStudents() {
+        TypedQuery<Course> query = entityManager.createQuery("SELECT C FROM Course C WHERE SIZE(C.students) >= 2", Course.class);
+        List<Course> courses = query.getResultList();
+        logger.info(String.format("Courses with at least 2 students -> {%s}", courses));
+    }
+
+    @Test
+    public void findCoursesAndOrderByAscStudents() {
+        TypedQuery<Course> query = entityManager.createQuery("SELECT C FROM Course C ORDER BY SIZE(C.students)", Course.class);
+        List<Course> courses = query.getResultList();
+        logger.info(String.format("Courses ordered by students -> {%s}", courses));
+    }
+
+    @Test
+    public void findCoursesAndOrderByDescStudents() {
+        TypedQuery<Course> query = entityManager.createQuery("SELECT C FROM Course C ORDER BY SIZE(C.students) DESC", Course.class);
+        List<Course> courses = query.getResultList();
+        logger.info(String.format("Courses ordered by students -> {%s}", courses));
+    }
+
+    @Test
+    public void findStudentsWithCertainPassportPattern() {
+        TypedQuery<Student> query = entityManager.createQuery("SELECT S FROM Student S WHERE S.passport.number LIKE '%17389%'", Student.class);
+        List<Student> students = query.getResultList();
+        logger.info(String.format("Students with certain passport pattern -> {%s}", students));
+    }
+
+    /**
+     * (INNER) JOIN -> SELECT C, S FROM Course C JOIN C.students S
+     * LEFT (OUTER) JOIN -> SELECT C, S FROM Course C LEFT JOIN C.students S
+     * CROSS JOIN -> SELECT C, S FROM Course C, Student S
+     */
+
+    @Test
+    public void join() {
+        // 배열의 형태로 반환합니다
+        Query query = entityManager.createQuery("SELECT C, S FROM Course C JOIN C.students S");
+        List<Object[]> resultList = query.getResultList();
+        logger.info(String.format("Results size -> {%s}", resultList.size()));
+        resultList.stream()
+                .forEach(result -> logger.info(String.format("Course{%s} Student{%s}", result[0], result[1])));
+    }
+
+    @Test
+    public void leftJoin() {
+        // 배열의 형태로 반환합니다
+        Query query = entityManager.createQuery("SELECT C, S FROM Course C LEFT JOIN C.students S");
+        List<Object[]> resultList = query.getResultList();
+        logger.info(String.format("Results size -> {%s}", resultList.size()));
+        resultList.stream()
+                .forEach(result -> logger.info(String.format("Course{%s} Student{%s}", result[0], result[1])));
+    }
+
+    @Test
+    public void crossJoin() {
+        // 배열의 형태로 반환합니다
+        Query query = entityManager.createQuery("SELECT C, S FROM Course C, Student S");
+        List<Object[]> resultList = query.getResultList();
+        logger.info(String.format("Results size -> {%s}", resultList.size()));
+        resultList.stream()
+                .forEach(result -> logger.info(String.format("Course{%s} Student{%s}", result[0], result[1])));
     }
 }
