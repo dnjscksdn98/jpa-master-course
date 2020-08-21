4b23f2c470bf7622645eeeb2d9acafa34023f459
Alex Won <dnjscksdn98@gmail.com>
2020-08-21 17:31:51 +0900

    feat: create repository using spring-data-jpa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,18 @@
 INSERT INTO courses(id, name, created_date, updated_date) VALUES(10001, 'Spring MSA', NOW(), NOW());
 INSERT INTO courses(id, name, created_date, updated_date) VALUES(10002, 'Spring Data JPA', NOW(), NOW());
 INSERT INTO courses(id, name, created_date, updated_date) VALUES(10003, 'Spring Boot', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10004, 'Spring Cloud', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10005, 'Microservices', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10006, 'Java Design Patterns', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10007, 'Docker', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10008, 'Kubernetes', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10009, 'Spring Security', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10010, 'Spring Batch', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10011, 'Javascript', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10012, 'Typescript', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10013, 'C++', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10014, 'Blockchain', NOW(), NOW());
+INSERT INTO courses(id, name, created_date, updated_date) VALUES(10015, 'Etherium', NOW(), NOW());
 
 INSERT INTO passports(id, number, created_date, updated_date) VALUES(30001, 'E17389', NOW(), NOW());
 INSERT INTO passports(id, number, created_date, updated_date) VALUES(30002, 'Z19479', NOW(), NOW());
