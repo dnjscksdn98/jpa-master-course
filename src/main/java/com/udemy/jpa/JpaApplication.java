ad8e87be1e9819c5925240525b3509a9b5d5ae76
Alex Won <dnjscksdn98@gmail.com>
2020-08-22 13:34:45 +0900

    feat: cache data using jpa-hibernate & ehcache

--- src/main/java/com/udemy/jpa/JpaApplication.java
+++ src/main/java/com/udemy/jpa/JpaApplication.java
@@ -1,6 +1,7 @@
 package com.udemy.jpa;
 
-import com.udemy.jpa.repository.EmployeeRepository;
+import com.udemy.jpa.models.Course;
+import com.udemy.jpa.repository.CourseSpringDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +9,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 
 @SpringBootApplication
 public class JpaApplication implements CommandLineRunner {
 
 	@Autowired
-	private EmployeeRepository employeeRepository;
+	private CourseSpringDataRepository courseRepository;
 
 	private final static Logger logger = LoggerFactory.getLogger(JpaApplication.class);
 
@@ -23,6 +26,7 @@ public class JpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+        Optional<Course> course = courseRepository.findById(10001L);
+        Optional<Course> course2 = courseRepository.findById(10001L);
 	}
 }
--- src/test/java/com/udemy/jpa/repository/CourseRepositoryTests.java
+++ src/test/java/com/udemy/jpa/repository/CourseRepositoryTests.java
@@ -79,4 +79,23 @@ class CourseRepositoryTests {
         logger.info(String.format("Review course [%s]", review.getCourse()));
     }
 
+    @Test
+    @Transactional
+    public void findByIdFirstLevelCache() {
+        Course course = courseRepository.findById(10001L);
+        logger.info("First course retrieved");
+
+        /**
+         * First Level Cache - Single Transaction
+         * 자동으로 관리와 실행이 되므로 별도의 설정이 필요 없음
+         * @Transactional 어노테이션이 없으면 데이터베이스로 매번 쿼리를 실행함 - 각각의 연산이 다른 트랜잭션이 되므로 더이상 하나의 트랜잭션이 아님
+         * 트랜잭션 내의 PersistenceContext 내에서의 엔티티에 대한 데이터가 캐시되므로 다시 데이터베이스로 쿼리를 실행하지 않음
+         */
+        Course course2 = courseRepository.findById(10001L);
+        logger.info("First course retrieved again");
+
+        assertEquals("Spring MSA", course.getName());
+        assertEquals("Spring MSA", course2.getName());
+    }
+
 }
